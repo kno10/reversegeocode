88a52284e0a6d6acbf7f5c978e8910cdad6d7d2b
Erich Schubert <kno10@users.noreply.github.com>
2015-12-08 13:41:58 +0100

    Fine-tune JMH benchmarks

--- src/bench/java/com/kno10/reversegeocode/bench/BenchmarkReverseGeocoder.java
+++ src/bench/java/com/kno10/reversegeocode/bench/BenchmarkReverseGeocoder.java
@@ -7,12 +7,15 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -22,80 +25,83 @@ import com.kno10.reversegeocode.query.ReverseGeocoder;
 
 /**
  * JMH Microbenchmarks
- * 
+ *
  * @author Erich Schubert
  */
-@State(Scope.Thread)
+@BenchmarkMode({ Mode.AverageTime, Mode.Throughput })
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Fork(1)
-@BenchmarkMode(Mode.Throughput)
-@OutputTimeUnit(TimeUnit.SECONDS)
-// @BenchmarkMode(Mode.AverageTime)
-// @OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 10, time = 1)
+@Measurement(iterations = 10, time = 1)
+@State(Scope.Thread)
 public class BenchmarkReverseGeocoder {
-	static final String FILENAME = "osm-20150126-0.01.bin";
+  @Param({ "data/osm-20150928-0.0005-2.bin", "data/osm-20150928-0.005-2.bin", //
+  "data/osm-20150928-0.05-2.bin", "data/osm-20150928-0.5-2.bin" })
+  public String filename;
 
-	ReverseGeocoder rgc;
+  ReverseGeocoder rgc;
 
-	@Setup(Level.Trial)
-	public void setup() throws IOException {
-		rgc = new ReverseGeocoder(FILENAME);
-	}
+  @Setup(Level.Trial)
+  public void setup() throws IOException {
+    if(filename == null) {
+      System.err.println("JMH bug? Param not initialized.");
+      System.exit(1);
+    }
+    rgc = new ReverseGeocoder(filename);
+  }
 
-	@TearDown
-	public void teardown() throws IOException {
-		rgc.close();
-	}
+  @TearDown
+  public void teardown() throws IOException {
+    rgc.close();
+  }
 
-	/**
-	 * Benchmark random number generation itself.
-	 * 
-	 * @return Sum
-	 */
-	@Benchmark
-	public int randomNumberGeneration() {
-		float f1 = (float) (Math.random() * 360. - 180.);
-		float f2 = (float) (Math.random() * 120. - 60.);
-		return (int) (f1 + f2);
-	}
+  /**
+   * Benchmark random number generation itself.
+   *
+   * @return Sum
+   */
+  @Benchmark
+  public int randomNumberGeneration() {
+    float f1 = (float) (Math.random() * 360. - 180.);
+    float f2 = (float) (Math.random() * 120. - 60.);
+    return (int) (f1 + f2);
+  }
 
-	@Benchmark
-	public int lookup() {
-		float f1 = (float) (Math.random() * 360. - 180.);
-		float f2 = (float) (Math.random() * 120. - 60.);
-		return rgc.lookup(f1, f2).length;
-	}
+  @Benchmark
+  public int lookup() {
+    float f1 = (float) (Math.random() * 360. - 180.);
+    float f2 = (float) (Math.random() * 120. - 60.);
+    return rgc.lookup(f1, f2).length;
+  }
 
-	@Benchmark
-	public int lookupUncached() {
-		float f1 = (float) (Math.random() * 360. - 180.);
-		float f2 = (float) (Math.random() * 120. - 60.);
-		return rgc.lookupEntryUncached(rgc.lookupUncached(f1, f2)).length;
-	}
+  @Benchmark
+  public int lookupUncached() {
+    float f1 = (float) (Math.random() * 360. - 180.);
+    float f2 = (float) (Math.random() * 120. - 60.);
+    return rgc.lookupEntryUncached(rgc.lookupUncached(f1, f2)).length;
+  }
 
-	@Benchmark
-	public int openQueryClose() throws IOException {
-		ReverseGeocoder lrgc = new ReverseGeocoder(FILENAME);
-		float f1 = (float) (Math.random() * 360. - 180.);
-		float f2 = (float) (Math.random() * 120. - 60.);
-		int ret = lrgc.lookup(f1, f2).length;
-		lrgc.close();
-		return ret;
-	}
+  @Benchmark
+  public int openQueryClose() throws IOException {
+    ReverseGeocoder lrgc = new ReverseGeocoder(filename);
+    float f1 = (float) (Math.random() * 360. - 180.);
+    float f2 = (float) (Math.random() * 120. - 60.);
+    int ret = lrgc.lookup(f1, f2).length;
+    lrgc.close();
+    return ret;
+  }
 
-	/**
-	 * Static main method.
-	 * 
-	 * @param args
-	 * @throws RunnerException
-	 */
-	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder() //
-				.include(BenchmarkReverseGeocoder.class.getSimpleName()) //
-				.warmupIterations(5)//
-				.measurementIterations(5)//
-				.forks(1)//
-				.build();
+  /**
+   * Static main method.
+   *
+   * @param args
+   * @throws RunnerException
+   */
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder() //
+    .include(BenchmarkReverseGeocoder.class.getSimpleName()) //
+    .build();
 
-		new Runner(opt).run();
-	}
+    new Runner(opt).run();
+  }
 }
--- src/test/java/com/kno10/reversegeocode/query/RunTestQueries.java
+++ src/test/java/com/kno10/reversegeocode/query/RunTestQueries.java
@@ -5,49 +5,60 @@ import java.util.Locale;
 
 /**
  * Simple class to perform a few test queries
- * 
+ *
  * @author Erich Schubert
  */
 public class RunTestQueries {
-	// Queries to run
-	static float[][] data = new float[][] {//
-	{ -73.9865812f, 40.7305991f }, // New York
-			{ 11.5754815f, 48.1372719f }, // Munich
-			{ 11.61406f, 48.06596f }, // Near Munich
-			{ 0f, 0f }, // Deep sea
-	};
+  // Queries to run
+  static float[][] data = new float[][] { //
+      { -73.9865812f, 40.7305991f }, // New York
+      { 11.5754815f, 48.1372719f }, // Munich
+      { 11.5945f, 48.1496f }, // Munich
+      { 11.6032448f, 48.1326788f }, // Munich Haidhausen
+      { 11.61406f, 48.06596f }, // Near Munich
+      { -77.03653f, 38.897676f }, // Washington DC
+      { -74.4264615f, 39.3569861f }, // Atlantic city
+      { -80.188441f, 25.790673f }, // Miami, FL
+      { 115.852662f, -31.9546529f }, // Australia
+      { 0f, 0f }, // Deep sea
+  };
 
-	public static void main(String[] args) {
-		try {
-			ReverseGeocoder rgc = new ReverseGeocoder(args[0]);
-			for (float[] f : data) {
-				String[] l = rgc.lookup(f[0], f[1]);
-				System.out.format(Locale.ROOT, "%8.4f %8.4f", f[0], f[1]);
-				for (String s : l) {
-					System.out.append("||").append(s);
-				}
-				System.out.append('\n');
-			}
-			for (int i = 0; i < 20; i++) {
-				float f1 = (float) (Math.random() * 360. - 180);
-				float f2 = (float) (Math.random() * 120. - 60);
-				String[] l = rgc.lookup(f1, f2);
-				if (l.length == 0) {
-					--i;
-					continue;
-				}
-				System.out.format(Locale.ROOT, "%8.4f %8.4f", f1, f2);
-				for (String s : l) {
-					System.out.append("||").append(s);
-				}
-				System.out.append('\n');
-			}
-			System.out.format(Locale.ROOT, "Number of entities: %d\n",
-					rgc.numentries);
-			rgc.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+  public static void main(String[] args) {
+    try {
+      ReverseGeocoder rgc = new ReverseGeocoder(args[0]);
+      for(float[] f : data) {
+        String[] l = rgc.lookup(f[0], f[1]);
+        System.out.format(Locale.ROOT, "%8.4f %8.4f %d", f[0], f[1], l != null ? l.length : 0);
+        if(l != null) {
+          for(String s : l) {
+            System.out.append("||").append(s);
+          }
+        }
+        else {
+          System.out.append("||null");
+        }
+        System.out.append('\n');
+      }
+      for(int i = 0; i < 20; i++) {
+        float f1 = (float) (Math.random() * 360. - 180);
+        float f2 = (float) (Math.random() * 120. - 60);
+        String[] l = rgc.lookup(f1, f2);
+        if(l == null || l.length == 0) {
+          --i;
+          continue;
+        }
+        System.out.format(Locale.ROOT, "%8.4f %8.4f %d", f1, f2, l != null ? l.length : 0);
+        for(String s : l) {
+          System.out.append("||").append(s);
+        }
+        System.out.append('\n');
+      }
+      System.out.format(Locale.ROOT, "Number of entities: %d\n", rgc.numentries);
+      rgc.close();
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
 }
